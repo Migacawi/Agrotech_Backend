183fa0000ec71602ba23654370aaa53d62104d5c
Seven_VII <nicoalejo0856@gmail.com>
2026-03-16 20:31:25 +0000

    middlewere para imagenes

--- src/routes/imagenesProducto_routes.js
+++ src/routes/imagenesProducto_routes.js
@@ -1,11 +1,31 @@
-const express = require('express');
-const router = express.Router();
-const imagenesProductoController = require('../controllers/imagenesproducto_controller');
+const router = require('express').Router();
+const { upload } = require('../middlewares/upload.middleware'); // ← aquí el cambio
+const authMiddleware = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
+const ownerMiddleware = require('../middlewares/ownerMiddleware');
+const {
+  subirImagenes,
+  eliminarImagen,
+  marcarComoPrincipal,
+  obtenerImagenesProducto
+} = require('../controllers/imagenesProducto_controller');
 
-router.get('/', imagenesProductoController.getImagenesProductos);
-router.get('/:id', imagenesProductoController.getImagenProductoById);
-router.post('/', imagenesProductoController.createImagenProducto);
-router.put('/:id', imagenesProductoController.updateImagenProducto);
-router.delete('/:id', imagenesProductoController.deleteImagenProducto);
+router.get('/producto/:productoId', obtenerImagenesProducto);
+
+router.post(
+  '/:productoId/subir',
+  authMiddleware,
+  roleMiddleware('vendedor'),
+  (req, res, next) => {
+    req.params.id = req.params.productoId;
+    next();
+  },
+  ownerMiddleware,
+  upload.array('imagenes', 5),
+  subirImagenes
+);
+
+router.delete('/:id', authMiddleware, roleMiddleware('vendedor'), eliminarImagen);
+router.put('/:id/principal', authMiddleware, roleMiddleware('vendedor'), marcarComoPrincipal);
 
 module.exports = router;
