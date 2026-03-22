be651f82aaf5f0c4e007a3522a65f1e364acc4d1
Migacawi <gabrielyovvanny@gmail.com>
2026-03-22 18:29:25 -0500

    Servicio se seed funcionando

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -3,25 +3,31 @@ const prisma = require("../prisma/client");
 
 const login = async (req, res) => {
   try {
-
     console.log("BODY:", req.body);
 
     const { email, password } = req.body;
 
     if (!email || !password) {
       return res.status(400).json({
-        message: "Email y password son requeridos"
+        message: "Email y password son requeridos",
       });
     }
 
     const user = await prisma.usuarios.findUnique({
       where: { Email: email },
-      include: { Rol: true }
+      include: { Rol: true },
     });
 
     if (!user) {
       return res.status(401).json({
-        message: "Usuario no encontrado"
+        message: "Usuario no encontrado",
+      });
+    }
+
+    // ✅ Verificar contraseña
+    if (user.PasswordHash !== password) {
+      return res.status(401).json({
+        message: "Contraseña incorrecta",
       });
     }
 
@@ -29,28 +35,25 @@ const login = async (req, res) => {
       {
         id: user.Id,
         email: user.Email,
-        rol: user.Rol.Nombre
+        rol: user.Rol.Nombre,
       },
       process.env.JWT_SECRET,
       {
-        expiresIn: "8h"
-      }
+        expiresIn: "8h",
+      },
     );
 
     res.json({ token });
-
   } catch (error) {
-
     console.error("ERROR LOGIN:", error);
 
     res.status(500).json({
       message: "Error en login",
-      error: error.message
+      error: error.message,
     });
-
   }
 };
 
 module.exports = {
-  login
+  login,
 };
