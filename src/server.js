073b6dd2ebe550e40623b6a2aa695e1bfeff294f
Migacawi <gabrielyovvanny@gmail.com>
2026-05-21 13:24:55 -0500

    arreglo pasarela de pago

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const imagenesProductoRoutes = require("./routes/imagenesProducto_routes");
 app.use(
   cors({
     origin: [
+      
       "http://localhost:5173", 
       "http://127.0.0.1:5173",
       "https://agrotech-proyectov2.onrender.com" // URL de tu frontend en producción
